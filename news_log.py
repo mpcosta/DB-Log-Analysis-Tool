4ea1ff9bc2724ad8d598d4c7da85609516a12369
Marco Costa <markkcosta@gmail.com>
2017-11-08 18:28:20 +0000

    Fixed Code to conform with PEP8 Style Recommendations

--- news_log.py
+++ news_log.py
@@ -4,21 +4,25 @@ import psycopg2
 
 DBNAME = "news"
 
+
 def get_most_popular_artc(x):
     """Prints out the X most popular articles of the news database"""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute("SELECT title, views FROM articles \
-               LEFT JOIN (SELECT path, count(*) AS views FROM log GROUP BY path) AS popular_articles \
+               LEFT JOIN (SELECT path, count(*) AS views \
+               FROM log GROUP BY path) AS popular_articles \
                ON path LIKE '%%' || slug \
                ORDER BY views DESC \
                LIMIT %s", (x,))
     query_results = c.fetchall()
     # Check Query List Size and Compare with size Requested in x param
     if len(query_results) < int(x):
-        print("\nUnfortunately this database does not contain %s Articles." % (x,))
-        x = len(query_results);
-        print("\nInstead we will be showing you all the %s Articles available.\n" % (x,))
+        print("\nUnfortunately this database \
+                does not contain %s Articles." % (x,))
+        x = len(query_results)
+        print("\nInstead we will be showing you \
+                all the %s Articles available.\n" % (x,))
     # Print out the X most popular articles
     print("\nThe %s Most Popular Articles of all time are:\n" % (x,))
     for title, views in query_results:
@@ -32,7 +36,8 @@ def get_most_popular_authors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute("SELECT name, sum(views) FROM authors, articles \
-               LEFT JOIN (SELECT path, count(*) AS views FROM log GROUP BY path) AS all_articles \
+               LEFT JOIN (SELECT path, count(*) AS views \
+               FROM log GROUP BY path) AS all_articles \
                ON path LIKE '%%' || slug \
                WHERE authors.id = articles.author GROUP BY authors.id")
     query_results = c.fetchall()
@@ -47,20 +52,27 @@ def get_most_err_req(x):
     """Prints out the days in which more than X %% of requests led to errors"""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("SELECT notOKStatus.dates, (notOKStatus.total*100.0/allStatus.total) AS percentColumn \
-               FROM (SELECT CAST(time AS DATE) AS dates, count(status) AS total FROM log \
-               WHERE status NOT LIKE 200 || '%%' GROUP BY dates) AS notOKStatus \
-               JOIN (SELECT CAST(time AS DATE) AS dates, count(status) AS total FROM log \
+    c.execute("SELECT notOKStatus.dates, \
+               (notOKStatus.total*100.0/allStatus.total) AS percentColumn \
+               FROM (SELECT CAST(time AS DATE) AS dates, \
+               count(status) AS total FROM log \
+               WHERE status NOT LIKE 200 || '%%' GROUP BY dates) \
+               AS notOKStatus \
+               JOIN (SELECT CAST(time AS DATE) AS dates, \
+               count(status) AS total FROM log \
                GROUP BY dates) AS allStatus \
-               ON notOKStatus.dates = allStatus.dates WHERE (notOKStatus.total*100.0/allStatus.total) > %s", (x,))
+               ON notOKStatus.dates = allStatus.dates \
+               WHERE (notOKStatus.total*100.0/allStatus.total) > %s", (x,))
     query_results = c.fetchall()
     # Check Query List Size and Compare with percentage Requested in x param
     if len(query_results) <= 0:
-        print("\nUnfortunately we could not find any results that led to more than %s Percent." % (x,))
+        print("\nUnfortunately we could not find any results \
+                that led to more than %s Percent." % (x,))
         print("Pleasy try again with a different percentage value. \n")
     else:
-        # Print out the days that more than X percent of requests lead to errors
-        print("\nThe Dates in which more than %s %% of requests led to errors are:\n" % (x,))
+        # Print out the days that more than X percent of requests lead to errs
+        print("\nThe Dates in which more than %s %% of requests \
+                led to errors are:\n" % (x,))
         for author, views in query_results:
             print("%s - %.2f %%" % (author, views))
         print("\n")
@@ -68,10 +80,10 @@ def get_most_err_req(x):
 
 
 def main():
-    get_most_popular_artc(3);
-    get_most_popular_authors();
-    get_most_err_req(1);
+    get_most_popular_artc(3)
+    get_most_popular_authors()
+    get_most_err_req(1)
 
 
 if __name__ == '__main__':
-    main();
+    main()
